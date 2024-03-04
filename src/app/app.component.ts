d98c0d6541e41d23665238e4fdee43bdad65e061
Caio Santos <caiosant10@gmail.com>
2024-03-04 19:13:48 -0300

    Testing integration with api, envs and service creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ScheduleService } from './feature/schedule/services/schedule.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  providers:[ScheduleService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'barberqueue-angular';
+  schedules: Array<any> = [];
+
+  constructor(private scheduleService: ScheduleService) { }
+
+  ngOnInit() {
+    this.getSchedules()
+  }
+
+  private getSchedules() {
+    this.scheduleService.getSchedules().subscribe(res => {
+      this.schedules = res;
+    });
+  }
 }
---
+++ src/app/feature/schedule/services/schedule.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../../enviroments/enviroment";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: 'root' })
+export class ScheduleService {
+    readonly BASE_URL = environment.apiUrl;
+
+    constructor(private http: HttpClient) { }
+
+    public getSchedules(): Observable<any> {
+        return this.http.get(`${this.BASE_URL}/schedules`);
+    }
+
+}
