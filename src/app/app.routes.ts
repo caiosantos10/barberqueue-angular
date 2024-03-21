8dfba939737b2a863fb82f6ca9c1da4372440b99
Caio Santos <caiosant10@gmail.com>
2024-03-21 19:02:17 -0300

    creating dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ScheduleListComponent } from './features/schedules/components/schedule-list/schedule.component';
 import { CreateScheduleComponent } from './features/schedules/components/create-schedule/create-schedule.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: 'home', component: ScheduleListComponent }, 
+    { path: 'home', component: DashboardComponent }, 
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'create-schedule', component: CreateScheduleComponent}
+    { path: 'agenda', component: ScheduleListComponent }, 
+    { path: 'marcar-hora', component: CreateScheduleComponent},
+    { path: '**', component: DashboardComponent}
 ];
